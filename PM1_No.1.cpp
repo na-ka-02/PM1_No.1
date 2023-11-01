09dae8724dec054691b6be70b096290bc2d14670
K023G1066@g.neec.ac.jp <K023G1066@g.neec.ac.jp>
2023-11-01 10:46:37 +0900

    課題6

--- PM1_No.1.cpp
+++ PM1_No.1.cpp
@@ -8,7 +8,10 @@ int main(){
    std::cout << "2023/10/11\n";
 
    std::cout << "プルせずにmasterを書き変える\n";
+
    std::cout << "プルせずにmasterを書き変える2\n";
+
+   std::cout << "課題6\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
